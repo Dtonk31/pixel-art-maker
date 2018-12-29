428d6c693b6287bf3de8c6584957b6938ccce368
Dustin Tonkin <dtonk31@icloud.com>
2018-12-29 17:13:00 -0500

    Initial commit
    feat's
    - pixelCanvas
      - makeGrid
      - clearGrid
      - adding color
      - removing color

---
+++ pixel-art-maker/app.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+    $('#sizePicker').submit(function(event) {
+        clearGrid();
+        makeGrid();
+        registerMouseHandlers();
+        event.preventDefault();
+    });
+});
+
+
+function makeGrid() {
+    let inputH = $('#inputHeight').val();
+    let inputW = $('#inputWidth').val();
+    let table = $('#pixelCanvas');
+
+    for (let r = 1; r <= inputH; r++) {
+        // create row
+        let row = $('<tr></tr>').appendTo(table);
+        for (let c = 1; c <= inputW; c++) {
+            // create table cells (columns)
+            row.append('<td></td>');
+        }
+    }
+}
+
+// Clearing a grid
+function clearGrid() {
+    $('#pixelCanvas').children().remove();
+}
+
+// adding color
+function registerMouseHandlers() {
+
+    let mouseIsDown = false;
+
+    $('td').on('mousemove', function() {
+        if (mouseIsDown) {
+            let color = $('#colorPicker').val();
+            $(this).css('backgroundColor', color);
+        }
+    });
+
+    $('td').on('mousedown', function() {
+        mouseIsDown = true;
+    });
+
+    $('td').on('mouseup', function() {
+        mouseIsDown = false;
+    });
+
+// adding color to single cell
+    $('td').on('click', function() {
+        let color = $('#colorPicker').val();
+        $(this).css('backgroundColor', color);
+    });
+
+// removing color from the cell
+    $('td').on('dblclick', function() {
+        $(this).css('background', 'none');
+    })
+}
